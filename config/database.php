72e71550a87372c1ed5a3eb8de35e47c32bc21b9
Alphonse Junior Elenga <elengaalphonse107@gmail.com>
2022-07-26 07:36:59 +0200

    test database

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 try {
-    $bdd= new PDO('mysql:host=localhost;dbname=formsujet;charset=utf8;','root','root');
+    //$bdd= new PDO('mysql:host=localhost;dbname=formsujet;charset=utf8;','root','root');
+    $bdd= new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=formsujet;charset=utf8;','b7b4e27a6f33b7','fb865da6');
 } catch (Exception $e) {
     die('Une erreur a été trouvée : ' . $e->getMessage());
 }
-    
