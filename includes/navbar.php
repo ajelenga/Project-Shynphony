47b8e94e8916e9eb0da9b1f3f8370021919ef12f
Alphonse Junior Elenga <elengaalphonse107@gmail.com>
2022-07-26 08:40:01 +0200

    main page  menu 3

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,3 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Forum test</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    <li class="nav-item">
-        <a class="nav-link" href="sujets.php">Les sujets</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="creation-sujet.php">Publier un sujet</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
 <nav class="navbar navbar-light navbar-expand-md">
                 <div class="container-fluid"><a class="navbar-brand" href="index.php">Forum test</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navcol-1">
