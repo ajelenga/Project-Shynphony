6f721545737033a9489f93e0ee3aca01b4a0ff4e
Alphonse Junior Elenga <elengaalphonse107@gmail.com>
2022-07-26 08:38:30 +0200

    main page  menu 2

--- includes/navbar.php
+++ includes/navbar.php
@@ -14,3 +14,15 @@
     </ul>
   </div>
 </nav>
+
+<nav class="navbar navbar-light navbar-expand-md">
+                <div class="container-fluid"><a class="navbar-brand" href="index.php">Forum test</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="navbar-nav">
+                            <li class="nav-item nav-item-font"><a class="nav-link active" href="index.php">Home</a></li>
+                            <li class="nav-item nav-item-font"><a class="nav-link active" href="sujets.php">Les sujets</a></li>
+                            <li class="nav-item nav-item-font"><a class="nav-link active" href="creation-sujet.php">Publier un sujet</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
