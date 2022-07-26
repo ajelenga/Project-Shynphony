bbcd4ac10c54c25ef7ce17f8f98087bfe3deeaad
Alphonse Junior Elenga <elengaalphonse107@gmail.com>
2022-07-26 08:11:02 +0200

    main page

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 <body>
     <?php include 'includes/navbar.php'; ?>
 
-    <p> Site de Test pour un test technique</p>
-    
+    <section class="top-page">
+        <header class="header">
+            <div class="landing-page">
+                <h2 class="big-title"> Site de Test pour un test technique</h2>
+            </div>
+        </header>
+    </section>
     <?php include 'includes/footer.php'; ?>
