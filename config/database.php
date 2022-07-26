32ec23296abfb6246cf3854a35e7e8f2be2aedf5
ajelenga <47526291+ajelenga@users.noreply.github.com>
2022-07-26 07:41:07 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 try {
     //$bdd= new PDO('mysql:host=localhost;dbname=formsujet;charset=utf8;','root','root');
-    $bdd= new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=formsujet;charset=utf8;','b7b4e27a6f33b7','fb865da6');
+    $bdd= new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_7de0654ff4e60e0;charset=utf8;','b7b4e27a6f33b7','fb865da6');
 } catch (Exception $e) {
     die('Une erreur a été trouvée : ' . $e->getMessage());
 }
